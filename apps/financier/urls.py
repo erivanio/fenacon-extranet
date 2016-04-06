4ed813bea65c11ca353d20bfef58b2a3882fa91c
Erivânio Vasconcelos <erivanio@MacBook-Pro-de-Erivanio.local>
2016-04-05 22:36:44 -0300

    criando pagina pra lista e editar reembolso

--- apps/financier/urls.py
+++ apps/financier/urls.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url
 
-from apps.financier.views import TravelRefundCreateView, TravelRefundListView
+from apps.financier.views import TravelRefundCreateView, TravelRefundListView, TravelRefundUpdateView
 
 urlpatterns = patterns('',
-    url(r'^criar-reembolso/$', TravelRefundCreateView.as_view(), name='create_refund'),
+    url(r'^solicitacao-reembolso/$', TravelRefundCreateView.as_view(), name='create_refund'),
+    url(r'^solicitacao-reembolso/editar/(?P<pk>\d+)/$', TravelRefundUpdateView.as_view(), name='update_refund'),
     url(r'^listar-reembolsos/$', TravelRefundListView.as_view(), name='list_refund'),
 )
--- apps/financier/views.py
+++ apps/financier/views.py
@@ -1,6 +1,12 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView
+from apps.core.models import History
 
 from apps.financier.forms import TravelRefundForm
 from apps.financier.models import TravelRefund
@@ -18,8 +24,18 @@ class TravelRefundCreateView(CreateView):
 
 class TravelRefundListView(ListView):
     model = TravelRefund
-    template_name = 'financier/list_refund.html'
+    template_name = 'financier/list-refund.html'
 
     @method_decorator(login_required(login_url='/'))
     def dispatch(self, *args, **kwargs):
         return super(TravelRefundListView, self).dispatch(*args, **kwargs)
+
+
+class TravelRefundUpdateView(UpdateView):
+    model = TravelRefund
+    form_class = TravelRefundForm
+    template_name = 'financier/create_refund.html'
+
+    def get_success_url(self):
+        messages.success(self.request, 'Solicitação modificada com sucesso!')
+        return reverse('list_refund')
